32c3e7b001b5203c41fd43a9f0a5155fc6ec522c
joel <joelseb@gmail,com>
2024-12-18 21:20:13 +0530

    tried creating arrays for objects

---
+++ src/Arrayanalysis.java
@@ -0,0 +1,13 @@
+
+public class Arrayanalysis {
+    public static void main(String args[]) {
+        int a = 10;
+        Animals[] list = new Animals[10];
+        list[0] = new Animals();
+        System.out.println(list[0].v);
+        Object[] objects = new Object[15];
+        objects[0]= new Animals();
+        objects[1] =new Dbfunctions();
+
+    }
+}
