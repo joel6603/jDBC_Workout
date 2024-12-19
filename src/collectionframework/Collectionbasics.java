2cfb4eb6fc361ea4495843dec9571c7ee97f52a8
joel <joelseb@gmail,com>
2024-12-19 13:42:45 +0530

    started using collection interface

---
+++ src/collectionframework/Collectionbasics.java
@@ -0,0 +1,12 @@
+package collectionframework;
+import java.util.ArrayList;
+import java.util.Collection;
+public class Collectionbasics {
+    public static void main(String args[]){
+        Collection<String> collection= new ArrayList<>();
+        collection.add("arigato");
+        collection.add("thank you");
+        collection.add("nanni");
+        System.out.println(collection);
+    }
+}
